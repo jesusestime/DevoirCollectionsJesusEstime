e4ad499382c071d1bfd03a88cba0e620905002a5
Pegasus03 <estimejesus@gmail.com>
2022-11-17 00:36:37 +0200

    View list with 10 items

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 Provinces_burundi = ["Province de Bubanza", "Province de Bujumbura Mairie", "Province de Bujumbura", "Province de Bururi", "Province de Cankuzo", "Province de Cibitoke", "Province de Gitega", "Province de Karuzi", "Province de Kayanza", "Province de Kirundo"]
+
+
+for p in Provinces_burundi :
+    print(p)
