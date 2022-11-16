907426efb5a0ad21fee6c781dd7542212ab564fd
Pegasus03 <estimejesus@gmail.com>
2022-11-17 00:52:20 +0200

    View tuple with 10 items

--- main.py
+++ main.py
@@ -26,3 +26,6 @@ print("\n Question II : Tuple")
 
 Provinces_burundi = ("Province de Bubanza", "Province de Bujumbura Mairie", "Province de Bujumbura", "Province de Bururi", "Province de Cankuzo", "Province de Cibitoke", "Province de Gitega", "Province de Karuzi", "Province de Kayanza", "Province de Kirundo")
 
+
+for p in Provinces_burundi:
+    print(p)
