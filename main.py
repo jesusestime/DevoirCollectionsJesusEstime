a0a5d49e9a832f0f8ea08e56d18761dd44c97485
Pegasus03 <estimejesus@gmail.com>
2022-11-17 01:02:59 +0200

    Views dictionnary with 10 items (keys and values)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("\n Question I : Liste")
+print("\nQuestion I : Liste\n")
 
-Provinces_burundi = ["Province de Bubanza", "Province de Bujumbura Mairie", "Province de Bujumbura", "Province de Bururi", "Province de Cankuzo", "Province de Cibitoke", "Province de Gitega", "Province de Karuzi", "Province de Kayanza", "Province de Kirundo"]
+Provinces_burundi = ["Province de Bururi","Province de Bubanza", "Province de Bujumbura Mairie", "Province de Bujumbura","Province de Cankuzo", "Province de Cibitoke", "Province de Gitega", "Province de Karuzi", "Province de Kayanza", "Province de Kirundo"]
 
 
 for p in Provinces_burundi :
@@ -35,7 +35,7 @@ del Provinces_burundi
 
 
 
-print("\n Question II : Tuple")
+print("\n Question II : Tuple\n")
 
 Provinces_burundi = ("Province de Bujumbura","Province de Bubanza", "Province de Bujumbura Mairie","Province de Bururi", "Province de Cankuzo", "Province de Cibitoke", "Province de Gitega", "Province de Karuzi", "Province de Kayanza", "Province de Kirundo")
 
@@ -46,3 +46,12 @@ for p in Provinces_burundi:
 print(Provinces_burundi[4])
 
 Provinces_burundi_sorted=sorted(Provinces_burundi)
+
+
+print("\n Question III : Dictionnaire")
+
+Provinces_burundi = {"Bur": "Province de Bururi","Buz":"Province de Bubanza","Bjm": "Province de Bujumbura Mairie","Bj":"Province de Bujumbura","Ckz" : "Province de Cankuzo","Cbt" : "Province de Cibitoke","Gtg": "Province de Gitega","Krz" : "Province de Karuzi","Kyz" :"Province de Kayanza","Krd" :"Province de Kirundo"}
+
+
+for key,value in Provinces_burundi.items():
+    print(key +" : "+ value)
